79ee799c33b11edec729e6025d003a09b1509e98
andregbrl7 <andrezoculoto@hotmail.com>
2024-01-02 13:42:30 -0300

    terminando mudanças

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -1,14 +1,8 @@
 import { test } from '@japa/runner'
 
-//test('display welcome page', async ({ client }) => {
- // const response = await client.get('/')
+// test('display welcome page', async ({ client }) => {
+//   const response = await client.get('/')
 
-//  response.assertStatus(200)
- // response.assertBodyContains({ hello: 'world' })
-//})
-
-//test ('nome do app', async ({client})=> {
-  //const resposta= await client.get('/')
-  //resposta.assertStatus(200)
-  //resposta.assertBodyContains ({app: 'favio-back'})
-//})
+//   response.assertStatus(200)
+//   response.assertBodyContains({ hello: 'world' })
+// })
